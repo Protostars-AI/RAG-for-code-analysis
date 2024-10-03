ff9b802dc6f9d3362980af5df060469728b59c3e
raoneg <rawan.gafar21@gmail.com>
2024-10-03 21:48:31 +0300

    setting the port to 5001

--- init.py
+++ init.py
@@ -114,4 +114,4 @@ def get_results(task_id):
 
 
 if __name__== "__main__":
-    app.run(debug=True)
+    app.run(host="127.0.0.1", port=5001, debug=True)
--- wsgi.py
+++ wsgi.py
@@ -9,4 +9,4 @@ logging.basicConfig(stream=sys.stderr, level=logging.INFO)
 from init import app
 
 if __name__== "__main__":
-    app.run(debug=True)
+    app.run(host="127.0.0.1", port=5001, debug=True)
