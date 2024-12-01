f348c2500f07a229f02e0b17df4646190243a253
raoneg <rawan.gafar21@gmail.com>
2024-12-01 17:59:33 +0200

    changed the name of the init file to rag_init and reflected changes in wsgi.py file

--- wsgi.py
+++ wsgi.py
@@ -6,7 +6,7 @@ import logging
 logging.basicConfig(stream=sys.stderr, level=logging.INFO)
 
 # Import and run the Flask app
-from init import app
+from rag_init import app
 
 if __name__== "__main__":
     app.run(host="127.0.0.1", port=5001, debug=True)
